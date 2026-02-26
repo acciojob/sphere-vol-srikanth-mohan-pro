2962b2de95eab8a55d3b5503e314ebbe8ecc4bd9
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-26 15:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+  let r=Number(document.getElementById("radius").value);
+	if(isNaN(r)||r<0){
+		document.getElementById("volume").value=NaN
+		return false;
+	};
+	document.getElementById("volume").value=((4/3)*Math.PI*r**3).toFixed(4);
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
